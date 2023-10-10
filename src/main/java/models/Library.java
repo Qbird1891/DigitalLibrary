faafd6e88fa47a18b492b22a5371fc02a656d474
Qbird1891 <qbirdsley@gmail.com>
2023-10-10 14:35:55 -0500

    - Started on project, added Library pojo in model package

---
+++ src/main/java/models/Library.java
@@ -0,0 +1,50 @@
+package models;
+
+
+/**
+ * @author Quinn Birdsley
+ * @author Evan Burnell
+ * CIS175 - Fall 2021
+ * Oct 10, 2023
+ */
+public class Library {
+	private int id;
+	private String title;
+	private String type;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+}
